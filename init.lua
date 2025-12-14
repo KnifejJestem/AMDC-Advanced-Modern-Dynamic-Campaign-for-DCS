f9930b88112e4d6b2a15824273231cc1ef729d6d
Mateusz Różyński <knifejjestem@gmail.com>
2025-12-14 15:00:05 +0100

    edited init.lua & zones.lua & AMDC Miz
    Made init.lua load an updated file of zones.lua. Made AMDC Miz load an always up to date file of init.lua.

--- init.lua
+++ init.lua
@@ -1,4 +1,22 @@
--- File for initializing all AMDC functions.
+--[[                                     
+                                        
+▄▄ ▄▄  ▄▄ ▄▄ ▄▄▄▄▄▄   ▄▄    ▄▄ ▄▄  ▄▄▄  
+██ ███▄██ ██   ██     ██    ██ ██ ██▀██ 
+██ ██ ▀██ ██   ██   ▄ ██▄▄▄ ▀███▀ ██▀██ 
+                                        
+--]]
 
--- Load zones module
-dofile("zones.lua")
+-- Change these variables to point to your files (THIS IS TO FIX LATER)
+
+local zones = [[C:\Users\knife\Desktop\AMDC for DCS\AMDC-for-DCS\zones.lua]]
+
+
+--[[                                                                                               
+                                                                                                
+▄▄     ▄▄▄   ▄▄▄  ▄▄▄▄     ▄▄▄▄  ▄▄▄  ▄▄▄▄  ▄▄▄▄▄   ▄▄   ▄▄  ▄▄▄  ▄▄▄▄  ▄▄ ▄▄ ▄▄    ▄▄▄▄▄  ▄▄▄▄ 
+██    ██▀██ ██▀██ ██▀██   ██▀▀▀ ██▀██ ██▄█▄ ██▄▄    ██▀▄▀██ ██▀██ ██▀██ ██ ██ ██    ██▄▄  ███▄▄ 
+██▄▄▄ ▀███▀ ██▀██ ████▀   ▀████ ▀███▀ ██ ██ ██▄▄▄   ██   ██ ▀███▀ ████▀ ▀███▀ ██▄▄▄ ██▄▄▄ ▄▄██▀ 
+                                                                                                
+--]]
+--dofile(zones)
+assert(loadfile(zones))()
